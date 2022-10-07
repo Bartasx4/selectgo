1674e2ff9c3fb5cffc31901fd80b13835f90f846
Bartek <bartasx@hotmail.com>
2022-10-07 12:33:42 +0200

    Added options and sliders

--- command.py
+++ command.py
@@ -1,45 +1,3 @@
-<<<<<<< HEAD
-
-
-class Command:
-	
-	def __init__(self, pokemon_count):
-		self.toggle_data = [f'#{index}' for index in range(pokemon_count)]
-		self.toggle_data |= {'kanto': 0, 'johto': 0, 'hoenn': 0, 'sinnoh': 0, 'unova': 0, 'kalos': 0, 'galar': 0, 'alola': 0}
-		self.toggle_data |= {'4*': 0, '3*': 0, '2*': 0, '1*': 0, '0*': 0,
-                             'shiny': 0, 'legendary': 0, 'mythical': 0, 'hyper beasts': 0, 'shadow': 0,
-                             'purified': 0, 'lucky': 0, 'traded': 0, 'hatched': 0, 'raid': 0, 'gbl': 0,
-                             'costume': 0, 'alolan': 0, 'galar': 0, 'onlyeggs': 0, 'defender': 0,
-                             'xlcandy': 0, 'evolvenew': 0, '0attack': 0, '0defense': 0, '0stamina': 0}
-		
-	@property
-	def command(self):
-		command = ''
-		for key, status in self.toggle_data.items():
-			if status == 0:
-				continue
-			if len(command) > 0:
-				command += '&'
-			if status == 2:
-				command += '!'
-			if '#' in key:
-				command += key[1:]
-			else:
-				command += key
-		return command
-	
-	@property	
-	def data(self):
-		return self.toggle_data
-		
-	def toggle(self, id_):
-	   # 0 - none, 1 - select, 2 - ignore
-	   toggle = self.toggle_data
-	   toggle[id_] += 1
-	   if toggle[id_] == 3:
-	       toggle[id_] = 0
-	   return toggle[id_]
-=======
 from assets import Assets
 
 
@@ -165,4 +123,3 @@ class Command:
     def load_cookie(self, cookie):
         for key, status in cookie.items():
             self.toggle_data[key] = status
->>>>>>> a4c9c82 (Added options and sliders. Need to add sliders to cookies)
--- static/js/load.js
+++ static/js/load.js
@@ -2,21 +2,6 @@ function load(url, myFunction, data='', method='POST') {
     var dfrd1 = $.Deferred();
     var xhttp = new XMLHttpRequest();
     var element = new Object();
-<<<<<<< HEAD
-    setTimeout(function(){
-        xhttp.onreadystatechange = function() {
-            if (this.readyState == 4 && this.status == 200) {
-                myFunction(this.responseText);
-            }
-        };
-        xhttp.open(method, url, true);
-        xhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-        xhttp.send(data);
-        dfrd1.resolve();
-    }, 2000);
-    return dfrd1.promise();
-   }
-=======
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             myFunction(this.responseText);
@@ -29,4 +14,3 @@ function load(url, myFunction, data='', method='POST') {
     return $.when(dfrd1).done(function(){
     }).promise();
 }
->>>>>>> a4c9c82 (Added options and sliders. Need to add sliders to cookies)
